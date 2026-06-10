621c5f397d78159fe1f19b75a3dc2bd98677dd61
danieldellabella2006-crypto <danieldellabella2006@gmail.com>
2026-06-10 11:35:18 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Jogando estuprando rosafa",
+        name: "Jogando rosafa na cama",
         // Activity type number (0 = Playing).
         type: 0, 
       },
